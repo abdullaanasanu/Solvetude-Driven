a2fde12b6afe44dbafd54e1fff32d199e41f7737
Abdulla Anas <abdullaanasanu@gmail.com>
2024-05-12 10:13:47 +0400

    Design fixes

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,8 +3,6 @@ import React from "react";
 
 // UI Imports
 import { Navbar } from "./navbar";
-
-import style from "./style.module.scss";
 import { Footer } from "./footer";
 
 // Component
@@ -12,7 +10,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <main className={style.container}>{children}</main>
+      <main>{children}</main>
       <Footer />
     </>
   );
